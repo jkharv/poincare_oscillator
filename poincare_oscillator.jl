1ac38f56ecc08a420b7b2c60551067826055b712
Jake Harvey <jakekharvey@gmail.com>
2021-09-26 23:46:45 -0400

    stuff pretty much actually works now

--- poincare_oscillator.jl
+++ poincare_oscillator.jl
@@ -48,31 +48,105 @@ function detect_period(ds::Dataset)::Integer
     return(period)
 end
 
-points = DataFrame(τ = Float64[], b = Float64[])
-for τ in 0.49:0.001:1.0
-    for b in 1.0:0.001:2.0
+function make_frame(τ_start, τ_end, b_start, b_end, k, stepsize)
+    
+    l = ReentrantLock()
+    points = DataFrame(τ = Float64[], b = Float64[], p = Float64[])
+    for τ in τ_start:stepsize:τ_end
+        Threads.@threads for b in b_start:stepsize:b_end
 
-        state0 = [1.0, 0.0, 0.0] # x, y, ϕ
-        parameters = [1000, τ, b] # k, τ, b
-        stim_system = DiscreteDynamicalSystem(stim_map, state0, parameters)
+            state0 = [1.0, 0.0, 0.0] # x, y, ϕ
+            parameters = [k, τ, b] # k, τ, b
+            stim_system = DiscreteDynamicalSystem(stim_map, state0, parameters)
         
-        # Discard 500 from the begining as a transient. 
-        t = trajectory(stim_system, 3021)[500:end] 
-        push!(points, [τ, b])
+           # Discard 500 from the begining as a transient. 
+           t = trajectory(stim_system, 800)[500:end]
+           period = detect_period(t)
+           lock(l)
+           try
+                push!(points, [τ, b, period])
+           finally
+                unlock(l)
+           end
+        end
+    end
+    return points
+end
+
+function categorize_period(p::AbstractFloat)
+
+    if p == 1.0
+        return(:orange)
+    end
+    if p == 2.0
+        return(:yellow)
+    end
+    if p == 3.0
+        return(:aquamarine)
+    end
+    if p == 4.0
+        return(:skyblue)
+    end
+    if p == 5.0
+        return(:fuchsia)
+    end
+    if p == 6.0
+        return(:red)
+    end
+    if p == 7.0
+        return(:goldenrod1)
+    end
+    if p == 8.0
+        return(:lightseagreen)
+    end
+    if p == -1.0
+        return(:lightcyan3)
+    end
+    if p > 8
+        return(:lightcyan3)
     end
 end
 
-state0 = [1.0, 0.0, 0] # x, y, ϕ
-parameters = [500, 0.5, 1.5] # k, τ, b
-stim_system = DiscreteDynamicalSystem(stim_map, state0, parameters)
+# Big plot
+points = make_frame(0.0, 1.0, 0.0, 2.0, 50, 0.001)
+transform!(points, :p => ByRow(categorize_period) => :colour)
 
-# Discard 500 from the begining as a transient. 
-t = trajectory(stim_system, 10000)[500:end]
+scatter(points[:,1], points[:,2], color = points[:,4], markersize = 0.75)
 
-a, b, c = columns(t)
+# Zoomed into upper left branch, interesting stuff here. 
+points = make_frame(0.25, 0.45, 0.9, 1.25, 500, 0.0001)
+transform!(points, :p => ByRow(categorize_period) => :colour)
 
-x = c
-y = circshift(c, 1)
+scatter(points[:,1], points[:,2], color = points[:,4], markersize = 0.75)
 
-scatter(y[2:end-1], x[2:end-1])
-detect_period(t)
+# Zoomed into the region at (tau=0.25, b=1)
+points = make_frame(0.24, 0.28, 0.99, 1.02, 500, 0.00001)
+transform!(points, :p => ByRow(categorize_period) => :colour)
+
+scatter(points[:,1], points[:,2], color = points[:,4], markersize = 0.75)
+
+
+# Animated figure of chaning k value.
+
+points = make_frame(0.0, 1.0, 0.0, 2.0, 50, 0.002)
+transform!(points, :p => ByRow(categorize_period) => :colour)
+   
+τ = Observable(points[:,1])
+b = Observable(points[:,2])
+c = Observable(points[:,4])
+
+scene = scatter(τ, b, color = c, markersize = 1.5)
+record(scene, "plots/vary_k.gif") do io
+    
+    for k = 50:-0.5:0
+
+        points = make_frame(0.0, 1.0, 0.0, 2.0, k, 0.002)
+        transform!(points, :p => ByRow(categorize_period) => :colour)
+        
+        τ[] = points[:,1]
+        b[] = points[:,2]
+        c[] = points[:,4]
+
+        recordframe!(io) # record a new frame
+    end
+end
